2aa65f86f84e5ff12d287f0a84a593d81513d7a8
Alberto Sousa <albertovssousa@gmail.com>
2024-03-14 18:42:05 -0300

    Connect the Frontend with your Backend using Socket.IO

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,7 @@
+import { Strapi } from '@strapi/strapi';
+import socketio from 'socket.io';
+import jwt from 'jsonwebtoken';
+
 export default {
   /**
    * An asynchronous register function that runs before
@@ -14,5 +18,30 @@ export default {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  bootstrap({ strapi }) {
+    const _strapi = strapi as Strapi;
+    const io = new socketio.Server(_strapi.server.httpServer);
+    
+    io.on('connection', (socket)=>{
+      console.log("a user connect");
+      
+      socket.on('searchBattle', (data)=>{
+        console.log('Search battle!');
+        
+        const myJwt = data.jwt;
+        const charId = data.characterId;
+        const jwtSecret = process.env.JWT_SECRET;
+
+        const decoded = jwt.verify(myJwt, jwtSecret) as jwt.JwtPayload;
+        const userId = decoded.id;
+
+        // add into the matchmaking queue
+      });
+
+      socket.on('sendTurn', (data)=>{
+        console.log('send TURN!');
+      })
+
+    })
+  },
 };
